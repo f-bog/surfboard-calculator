38e0ea9c3920f041125ba9448f6d2ddd38de4453
Fred Bogdanoff <fredericks.webdesigns@gmail.com>
2020-09-03 21:39:06 +1000

    Removed Surfboard Image

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Calculator from './components/Calculator';
 import './App.css';
-import surfboard from './images/surfboard-icon.png';
 
 function App() {
   return (
@@ -10,8 +9,6 @@ function App() {
       <h1>Surf Board Volume Calculator</h1>
       <div className='App'>
         <Calculator />
-
-        <img src={surfboard} alt='surfboard' width='70px;' height='70px;'></img>
       </div>
     </>
   );
